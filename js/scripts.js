42ed5b8d0ada5a38696a04c91c6b30fd762e8d7e
dusty mccord and josh Hellman <dusty@>
2020-01-23 11:32:59 -0800

    first spec of passing along number as been achived.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
+  var translated = [];
 
+  function makePL(word) {
+    //do the magic
+    //return result;
+  }
+
+  function printToDom(item) {
+    var result = item.join(" ");
+    $("#result").text(result);
+  }
+
+  //Button function
+  $("form").submit(function(event) {
+    event.preventDefault()
+    var words = $("#pig-latin-input").val().split(" ")
+
+    words.forEach(function(word){
+      if(isNaN(word)){
+        translated.push(makePL(word))
+      } else {
+        translated.push(word)
+      }
+    })//end foreach.
+    printToDom(translated);
+  })
+  
 });
 
